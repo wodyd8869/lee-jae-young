5f59b0dcfda93007dbe85e7232c683fa75340121
dlwodyd8869@naver.com <dlwodyd8869@naver.com>
2019-10-16 16:41:29 +0900

    4564

--- 소스1.cpp
+++ 소스1.cpp
@@ -1,18 +1,13 @@
-#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
 
-int main()
-{
-	int num;
-	printf("4자리 정수 입력 :");
-
-	scanf_s("%d", &num, sizeof(int));
-
-	printf("%10d\n", num);
-	printf("%010d\n", num);
-	printf("%0+10d\n", num);
-	printf("%-10d\n", num);
-	printf("%10o\n", num);
-	printf("%0#10o\n", num);
-	printf("%10x\n", num);
-	printf("%0#10x\n", num);
+int main() {
+	double a, b, c;
+	double d, e, f;
+	printf("섭씨온도를 소수로 3개 입력하시오:\n");
+	scanf("%lf %lf %lf", &a, &b, &c);
+	d = (9.0 / 5.0) * a + 32.0;
+	e = (9.0 / 5.0) * b + 32.0;
+	f = (9.0 / 5.0) * c + 32.0;
+	printf("%.4f\n %.4f\n %.4f\n", d, e, f);
 }
